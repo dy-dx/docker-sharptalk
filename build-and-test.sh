8dc0dfee0300bce2ee3c17e8b8df3821709e7a3d
Chris Kim <christopherjkim@gmail.com>
2017-08-02 19:04:05 -0400

    use alpine with mono

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
+set -e
 
 tag="sharptalk:latest"
 
-docker build -t "$tag" .
+docker build -f Dockerfile -t "$tag" .
 
 msg=${1:-"hello world"}
 
@@ -10,4 +11,4 @@ msg=${1:-"hello world"}
 #   ffmpeg -y -f s16le -ar 11025 -ac 1 -i asdf.pcm asdf.wav && \
 #   play asdf.wav
 
-echo "$msg" | ./docker-run.sh | play -b 16 -e signed-integer -L -r 11025 -c 1 -t raw -
+echo "$msg" | tag="$tag" ./docker-run.sh | play -b 16 -e signed-integer -L -r 11025 -c 1 -t raw -
